161e11aa60a6f813f78eb9ee7937957f27beba81
Bunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-11-21 23:18:31 +0200

    Added take a break system & minor changes

--- timerEngine.js
+++ timerEngine.js
@@ -14,7 +14,7 @@ module.exports = (client) => {
       if (interaction.customId === "devTimerA") {
         if (interaction.user.id !== "333644367539470337")
           return interaction.reply({
-            content: `Fuck off will you`,
+            content: `Developer only`,
             ephemeral: true
           });
         const timerData = await timerSchema.findOne({
@@ -45,7 +45,7 @@ module.exports = (client) => {
       } else if (interaction.customId === "devTimer") {
         if (interaction.user.id !== "333644367539470337")
           return interaction.reply({
-            content: `Fuck off will you`,
+            content: `Developer only`,
             ephemeral: true
           });
         const timerData = await timerSchema.findOne({
@@ -60,9 +60,18 @@ module.exports = (client) => {
 
         const startedAtUNIX = timerData.intiationTime.getTime();
 
-        const timeElapsed = (Date.now() - startedAtUNIX) / 1000;
+        let breakTime = 0;
+        let readableBreakTime;
+
+        if (timerData.sessionBreakTime) {
+          breakTime = timerData.sessionBreakTime;
+          readableBreakTime = msToTime(breakTime * 1000);
+        } else readableBreakTime = "0 Seconds";
+
+        const timeElapsed = (Date.now() - startedAtUNIX) / 1000 - breakTime;
 
         const oldAvg = timerData.timeSpent / timerData.numberOfStarts;
+
         const newAvg =
           (timerData.timeSpent + timeElapsed) / (timerData.numberOfStarts + 1);
 
@@ -74,9 +83,9 @@ module.exports = (client) => {
           .setTitle(`🕛 Session Ended`)
           .setColor(colours.DEFAULT)
           .setDescription(
-            `Time Elapsed:\n${msToTime(timeElapsed * 1000)} [${Math.round(
+            `• Time Elapsed:\n${msToTime(timeElapsed * 1000)} [${Math.round(
               timeElapsed
-            ).toLocaleString()} seconds]\n\nAverage Session Time Movement: ${averageDifference.toLocaleString()}s [${readableDifference}]`
+            ).toLocaleString()} seconds]\n• Break Time: ${readableBreakTime} [${breakTime}]\n\n• Average Study Session Time Movement: ${averageDifference.toLocaleString()}s [${readableDifference}]`
           )
           .setTimestamp();
 
@@ -87,9 +96,50 @@ module.exports = (client) => {
 
         return timerData.updateOne({
           intiationTime: null,
+          sessionBreakTime: null,
+          breakTimerStart: null,
           numberOfStarts: timerData.numberOfStarts + 1,
           timeSpent: Math.round(timerData.timeSpent + timeElapsed),
           lastSessionTime: timeElapsed
+        });
+      } else if (interaction.customId === "unpauseTimer") {
+        if (interaction.user.id !== "333644367539470337")
+          return interaction.reply({
+            content: `Developer only`,
+            ephemeral: true
+          });
+
+        const timerData = await timerSchema.findOne({
+          userID: interaction.user.id
+        });
+
+        if (!timerData || timerData.intiationTime === null) {
+          return interaction.reply({
+            content: `There's no previous recorded start time.`
+          });
+        }
+
+        const startedAtUNIX = timerData.breakTimerStart.getTime();
+
+        const timeElapsed = (Date.now() - startedAtUNIX) / 1000;
+
+        const readableTimeElapsed = msToTime(timeElapsed * 1000);
+
+        await timerData.updateOne({
+          breakTimerStart: null,
+          sessionBreakTime: timerData.sessionBreakTime + timeElapsed,
+          breakTime: timerData.breakTime + timeElapsed
+        });
+
+        const timerUnpaused = new MessageEmbed()
+          .setTitle(`🕜 Timer Un-Paused`)
+          .setColor(colours.DEFAULT)
+          .setDescription(
+            `• Break Duration: ${readableTimeElapsed} [${timeElapsed.toFixed(2)} s]`
+          );
+
+        return interaction.reply({
+          embeds: [timerUnpaused]
         });
       }
     }
