9d405550701d8152a34297d709bd074d8816444c
Bunnys <78443317+DepressedBunnys@users.noreply.github.com>
2022-11-26 14:49:04 +0200

    Added new fields

---
+++ timer schema.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+
+const TimerSchema = new Schema(
+  {
+    userID: String,
+    messageID: String,
+    intiationTime: {
+      type: Date,
+      default: null
+    },
+    numberOfStarts: {
+      type: Number,
+      default: 0
+    },
+    timeSpent: {
+      type: Number,
+      default: 0
+    },
+    longestSessionTime: {
+      type: Number,
+      default: 0
+    },
+    lastSessionTime: Number,
+    lastSessionDate: Date,
+    breakTime: {
+      type: Number,
+      default: 0
+    },
+    totalBreaks: {
+      type: Number,
+      default: 0
+    },
+    sessionBreaks: {
+      type: Number,
+      default: 0
+    },
+    sessionBreakTime: {
+      type: Number,
+      default: 0
+    },
+    breakTimerStart: Date,
+    breakMessageID: String
+  },
+  {
+    collection: "Timer data"
+  }
+);
+
+module.exports = model("Timer data", TimerSchema);
