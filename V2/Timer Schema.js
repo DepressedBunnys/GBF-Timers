5a3ef9eaf6b97905ed3a5903cc2b06f9668b7419
Bunnys <78443317+DepressedBunnys@users.noreply.github.com>
2023-01-22 01:51:57 +0200

    Added semester naming, resets and other

---
+++ V2/Timer Schema.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require("mongoose");
+
+const TimerSchema = new Schema(
+  {
+    userID: String,
+    messageID: String,
+    seasonName: String,
+    startTime: {
+      type: Array,
+      default: []
+    },
+    intiationTime: {
+      type: Date,
+      default: null
+    },
+    numberOfStarts: {
+      type: Number,
+      default: 0
+    },
+    timeSpent: {
+      type: Number,
+      default: 0
+    },
+    longestSessionTime: {
+      type: Number,
+      default: 0
+    },
+    sessionLengths: {
+      type: Array,
+      default: []
+    },
+    lastSessionTime: Number,
+    lastSessionDate: Date,
+    breakTime: {
+      type: Number,
+      default: 0
+    },
+    totalBreaks: {
+      type: Number,
+      default: 0
+    },
+    sessionBreaks: {
+      type: Number,
+      default: 0
+    },
+    sessionBreakTime: {
+      type: Number,
+      default: 0
+    },
+    breakTimerStart: Date,
+    breakMessageID: String
+  },
+  {
+    collection: "Timer data"
+  }
+);
+
+module.exports = model("Timer data", TimerSchema);
