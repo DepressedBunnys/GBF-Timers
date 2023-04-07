cfd9e4281dd6b05ddbbc6301186c7727cd25abe9
Bunnys <78443317+DepressedBunnys@users.noreply.github.com>
2023-04-08 00:23:06 +0200

    New fields for upcoming updates

--- V2/User Profile Schema.js
+++ V2/User Profile Schema.js
@@ -3,13 +3,37 @@ const { Schema, model } = require("mongoose");
 const UserProfileSchema = new Schema(
   {
     userID: String,
+    privateProfile: {
+      type: Boolean,
+      default: false
+    },
     cash: {
+      type: Number,
+      default: 0
+    },
+    bank: {
       type: Number,
       default: 1500
     },
     dunkelCoins: {
       type: Number,
       default: 500
+    },
+    totalEarned: {
+      type: Number,
+      default: 1500
+    },
+    RP: {
+      type: Number,
+      default: 0
+    },
+    Rank: {
+      type: Number,
+      default: 1
+    },
+    badges: {
+      type: Array,
+      default: []
     },
     dailyCooldown: Date,
     //type: Date,
